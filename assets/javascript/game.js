caa4267d8ee733b485a8a0be4bee26fe50d146e5
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-07-15 23:09:38 -0600

    Updated final game logic and removed test alerts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,36 @@
-var targetNumber = Math.round(Math.random() * 100);
-
-  $("#pointGoal").text(targetNumber);
-
+  
   var pointTotal = 0;
-
   var pointValues = [10, 2, 3, 8];
-
   var crystalPicArray = ['assets/images/blue.png', 'assets/images/green.png', 'assets/images/red.png', 'assets/images/yellow.png']
+  var wins = 0;
+  var losses = 0;
+  var targetNumber;
 
-  
 
-  
+  var startGame = function (){
+
+    targetNumber = Math.round(Math.random() * 100);
+    $("#pointGoal").text(targetNumber);
+    pointTotal = 0;
+
+  };
+
+
+  startGame();
 
   
   for (var i = 0; i < pointValues.length; i++) {
 
      
     var imageCrystal = $("<img class='crystalImage'>");
-
-    imageCrystal.attr("src", crystalPicArray[i]);
-        
+    imageCrystal.attr("src", crystalPicArray[i]);        
     imageCrystal.attr("crystalValue", pointValues[i]);
-
     $("#crystals").append(imageCrystal);
 
 
   }
 
   
-
   $(".crystalImage").on("click", function() {
 
     
@@ -39,15 +41,29 @@ var targetNumber = Math.round(Math.random() * 100);
     pointTotal += crystalPoint;
 
     
-    
-    alert("New score: " + pointTotal);
-
     if (pointTotal === targetNumber) {
-      alert("You win!");
+      wins++;
+      startGame();
     }
 
     else if (pointTotal >= targetNumber) {
-      alert("You lose!!");
+      losses++;
+      startGame();
     }
+
+
+    $("#wins").text("Wins:   " + wins);
+    $("#losses").text("Losses: " + losses);
+    $("#points").text("You have " + pointTotal + " points");
+
+
+  });
+
+
+
+
+  $("#start").on("click", function() { 
+
+    startGame();
 
   });
